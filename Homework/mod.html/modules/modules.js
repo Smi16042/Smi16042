6dbc7ac912e5440ab74d804e2835463cc6142778
Smi16042 <AustinSmith950@gmail.com>
2016-11-05 19:00:19 -0700

    pls

---
+++ Homework/mod.html/modules/modules.js
@@ -0,0 +1,11 @@
+$(function(){
+    $("#page-header").load("mod.html/modules/modules.js");\
+    });
+
+$(function(){
+    $("#pagenav") .load("mod.html/modules/modules.js");\
+     });
+    
+$(function(){
+    $("#page-footer").load("mod.html/modules/modules.js");\
+});
--- Homework/modules/modules.js
+++
@@ -1,5 +0,0 @@
-$(function() {
-    $("#page-header").load("../homework/modules/header.html");
-    $("#page-nav").load("nav.html"):
-    $("#page-footer").load("footer.html"):
-});
