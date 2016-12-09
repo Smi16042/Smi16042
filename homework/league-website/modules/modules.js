c9c1c5b6de41a09333f444aaef524683de17794e
smi16042 <AustinSmith950@gmail.com>
2016-12-08 18:07:05 -0800

    Revert "oh so many main website changes"
    This reverts commit 2d7b7b62bcdbd9b7c50f4db32758f2aa97aac17a.

--- homework/league-website/modules/modules.js
+++
@@ -1,5 +0,0 @@
-$(function(){
-    $("#myheader").load("../modules/header.html");   
-    $("#mynav").load("../modules/nav.html");
-    $("#myfooter").load("../modules/footer.html");
-});
---
+++ homework/mod/modules/modules.js
@@ -0,0 +1,5 @@
+$(function(){
+    $("#page-header").load("header.html");   
+    $("#pagenav").load("nav.html");
+    $("#page-footer").load("footer.html");
+});
